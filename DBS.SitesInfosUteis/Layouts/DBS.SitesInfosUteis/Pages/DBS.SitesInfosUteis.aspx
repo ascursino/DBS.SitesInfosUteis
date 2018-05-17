77a6a04e4c57a6e1aea070da593cc983e33cf22d
Andréa Cursino <ascursino@gmail.com>
2018-05-17 19:29:22 +1200

    Add project files.

---
+++ DBS.SitesInfosUteis/Layouts/DBS.SitesInfosUteis/Pages/DBS.SitesInfosUteis.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DBS.SitesInfosUteis.aspx.cs" Inherits="DBS.SitesInfosUteis.Layouts.DBS.SitesInfosUteis.DBS" DynamicMasterPageFile="~masterurl/default.master" %>
+
+<asp:Content ID="PageHead" ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
+
+</asp:Content>
+
+<asp:Content ID="Main" ContentPlaceHolderID="PlaceHolderMain" runat="server">
+    <asp:Label runat="server" ID = "lblErro" ForeColor="Red"></asp:Label>
+
+    <p class="subtitulo_pagina">:: Sites e Informações Úteis</p>
+    <p>&nbsp;</p>
+    
+    <p class="texto_pagina">Selecione a área de interesse: 
+      <asp:DropDownList ID="cboBusca" runat="server" CssClass="texto_pagina">
+      </asp:DropDownList>
+      
+      <asp:Button ID="btnBusca" runat="server" Text="Exibir" OnClick="btnBusca_OnClick" />
+    </p>
+    
+    <table width="100%" border="0" cellpadding="0" cellspacing="1" class="grid_tabela">
+      <tr class="grid_cabecalho">
+        <td width="39%">Descrição</td>
+        <td>Telefone</td>
+        <td width="41%">Site</td>
+      </tr>
+
+      <asp:Literal ID="ltrConteudo" runat="server" />
+      
+      <!--
+      <tr class="grid_conteudo">
+        <td>descricao</td>
+        <td>telefone</td>
+        <td>site</td>
+      </tr>
+      -->
+
+    </table>
+    <p>&nbsp;</p>
+
+</asp:Content>
+
---
+++ DBS.SitesInfosUteis/Layouts/DBS.SitesInfosUteis/Pages/DBS.SitesInfosUteis.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Web;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.WebControls;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace DBS.SitesInfosUteis.Layouts.DBS.SitesInfosUteis
+{
+    public partial class DBS : LayoutsPageBase
+    {
+        SPWeb web = SPContext.Current.Site.RootWeb;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsPostBack == false)
+                {
+                    MontaCboBusca();
+
+                    MontaGridConteudo("");
+                }
+            }
+            catch (Exception ex)
+            {
+                lblErro.Text = ex.ToString();
+            }
+        }
+
+        public void MontaGridConteudo(string varParamBusca)
+        {
+            string varLinha = "";
+
+            //Acessando a lista
+            SPList lstLista = web.Lists["SitesInfo"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+
+            if (varParamBusca.ToString() == "" || varParamBusca.ToString() == "Todas")
+            {
+                oQuery.Query =  "<OrderBy>" +
+                                "   <FieldRef Name='Title' Ascending='True' />" +
+                                "</OrderBy>";
+            }
+            else 
+            {
+                oQuery.Query =  "<Where>" +
+                                "   <Eq>" +
+                                "        <FieldRef Name='_x00c1_rea_x0020_de_x0020_Intere' /><Value Type='Choice'>" + varParamBusca.ToString() + "</Value>" +
+                                "    </Eq>" +
+                                "</Where>" +
+                                "<OrderBy>" +
+                                "   <FieldRef Name='Title' Ascending='True' />" +
+                                "</OrderBy>";
+            }
+
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            SPListItemCollection collListItems = lstLista.GetItems(oQuery);
+
+            //Recupera o item
+            foreach (SPListItem oListItem in collListItems)
+            {
+                StringBuilder sbLinha = new StringBuilder();
+
+                ////Monta linha da grid com os resultados
+                sbLinha.Append("<tr class='grid_conteudo'>");
+                sbLinha.Append("<td>&nbsp;" + AjustaCampo(oListItem["Title"].ToString()) + "</td>");
+
+                if (oListItem["Telefone"] != null)
+                {
+                    sbLinha.Append("<td align='center'>" + AjustaCampo(oListItem["Telefone"].ToString()) + "</td>");
+                }
+                else
+                {
+                    sbLinha.Append("<td></td>");
+                }
+                sbLinha.Append("<td>&nbsp;<a href='" + AjustaCampo(oListItem["Conteudo"].ToString()) + "' target='blank'>" + AjustaCampo(oListItem["Conteudo"].ToString()) + "</a></td>");
+                sbLinha.Append("</tr>");
+
+                varLinha += sbLinha.ToString();
+            }
+            
+            ltrConteudo.Text = varLinha.ToString();
+        }
+
+        public void MontaCboBusca()
+        {
+            //Acessando a lista
+            SPList lstLista = web.Lists["SitesInfo"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+
+            oQuery.ViewFields = "<FieldRef Name='_x00c1_rea_x0020_de_x0020_Intere'/>";
+            oQuery.Query =  "<OrderBy>" +
+                            "   <FieldRef Name='_x00c1_rea_x0020_de_x0020_Intere' Ascending='True' />" +
+                            "</OrderBy>";
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            //Criadas datatables e dataview para utilizar o distinct nos resultados, a Query CAML por si só não possui esse comando.
+            DataTable dtItemsColl = lstLista.GetItems(oQuery).GetDataTable();
+            DataView dvView = new DataView(dtItemsColl);
+            DataTable dtItemsList = dvView.ToTable(true, "_x00c1_rea_x0020_de_x0020_Intere"); //define que os resultados são distinct
+
+            //Limpa combo antes de popular os resultados
+            cboBusca.Items.Clear();
+            
+            cboBusca.Items.Add("Todas");
+
+            foreach (DataRow rowItems in dtItemsList.Rows)
+            {
+                ////Monta combo com os resultados
+                ListItem ItemCbo = new ListItem();
+                ItemCbo.Value = AjustaCampo(rowItems["_x00c1_rea_x0020_de_x0020_Intere"].ToString());
+                ItemCbo.Text = AjustaCampo(rowItems["_x00c1_rea_x0020_de_x0020_Intere"].ToString());
+
+                cboBusca.Items.Add(ItemCbo);
+            }
+        }
+
+        public string AjustaCampo(string vCampo)
+        {
+            if (vCampo.IndexOf(";#") > 0)
+            {
+                vCampo = vCampo.Substring(vCampo.IndexOf("#") + 1);
+            }
+            return vCampo;
+        }
+
+        protected void btnBusca_OnClick(object sender, EventArgs e)
+        {
+            MontaGridConteudo(cboBusca.SelectedValue.ToString());
+        }
+
+    }
+}
